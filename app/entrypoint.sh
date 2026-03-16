18203a4e80fbd719fe8f27f0829ee9178cb6180e
tun <username@users.noreply.github.com>
2026-03-16 14:00:19 +0300

    create admin user automatically

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -2,4 +2,5 @@
 
 python manage.py wait_for_db
 python manage.py migrate
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); username='admin'; password='dörtmevsim35AA'; email='admin@example.com'; User.objects.filter(username=username).exists() or User.objects.create_superuser(username, email, password)"
 python manage.py runserver 0.0.0.0:8000
